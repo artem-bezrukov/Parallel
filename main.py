c4e6f1ee5a5f204b340129c4f07e563c7d8f6d90
maximtretyakov <max.tret02@gmail.com>
2022-12-17 12:06:33 +0300

    Несколько мелких улучшений
    Заменил a на cash. Заменил checkbutton на radiobutton. Попытался добавить смену цвета progress_bar в соответствии с цветов сиропа(не работает)

--- main.py
+++ main.py
@@ -4,51 +4,95 @@ from time import sleep
 
 
 class Main(tk.Frame):
-    a = 0
+    cash = 0
     label_money = None
-    no_sirop = False
 
-    def __init__(self):
+    no_syrup = "no_syrup "
+    cherry = "cherry"
+    lemon = "lemon"
+    syrup_selected = {"no_syrup": False, 'cherry': False, 'lemon': False}
+    price = {"no_syrup": 10, 'cherry': 20, 'lemon': 15}
+
+    def __init__(self, root):
         super().__init__()
         self.initUI()
         self.master.title('Газировка')
         self.master.geometry('250x400+900+500')
 
     def initUI(self):
+
         self.columnconfigure([0, 1, 2, 3], weight=1, minsize=400, pad=3)
         self.rowconfigure([0, 1, 2, 3, 4], weight=1, minsize=250, pad=3)
 
-        self.label_money = tk.Label(root, text=self.a, font=('Arial', 14))
+        self.label_money = tk.Label(self.master, text=self.cash, font=('Arial', 14))
         self.label_money.grid(row=7, column=3)
-        #label_message = tk.Label(root, text='Автомат с газировкой', font=('Arial', 14))
-        #label_message.place(x=320, y=50)
+        # label_message = tk.Label(root, text='Автомат с газировкой', font=('Arial', 14))
+        # label_message.place(x=320, y=50)
 
-        nosirop_checkbutton = tk.Checkbutton(text="Без сиропа")
-        nosirop_checkbutton.grid(row=3, column=3, sticky='w')
+        syrup = tk.StringVar()
+        syrup.set(self.no_syrup)
+        nosyrup_radiobutton = tk.Radiobutton(text=f"Без сиропа {self.price['no_syrup']}", value=self.no_syrup,
+                                             variable=syrup,
+                                              command=self.choose_syrup(self.no_syrup))
+        nosyrup_radiobutton.grid(row=3, column=3, sticky='w')
 
-        lemonsirop_checkbutton = tk.Checkbutton(text="Лимонный сироп")
-        lemonsirop_checkbutton.grid(row=4, column=3, sticky='w')
+        lemonsyrup_radiobutton = tk.Radiobutton(text=f"Лимонный сироп {self.price['lemon']}", value=self.lemon,
+                                                variable=syrup,
+                                                 command=self.choose_syrup(self.lemon))
+        lemonsyrup_radiobutton.grid(row=4, column=3, sticky='w')
 
-        cherysirop_checkbutton = tk.Checkbutton(text="Вишнёвый сироп")
-        cherysirop_checkbutton.grid(row=5, column=3, sticky='w')
+        cherrysyrup_radiobutton = tk.Radiobutton(text=f"Вишнёвый сироп {self.price['cherry']}", value=self.cherry,
+                                                 variable=syrup,
+                                                  command=self.choose_syrup(self.cherry))
+        cherrysyrup_radiobutton.grid(row=5, column=3, sticky='w')
 
-        # Хочу сдать мартынова
-        btn_money = tk.Button(root, text='Пополнить баланс', command=self.money_get)
+        btn_money = tk.Button(self.master, text='Пополнить баланс', command=self.money_get)
         btn_money.grid(row=8, column=3, sticky='s')
 
     def money_get(self):
-        self.a += 10
-        self.label_money.config(text=self.a)
+        self.cash += 10
+        self.label_money.config(text=self.cash)
 
-        if self.a > 0:
-            btn_go = tk.Button(root, text='Налить', command=self.progress)
+        if self.cash > 0:
+            btn_go = tk.Button(self.master, text='Налить', command=self.fill_glass())
             btn_go.grid(row=6, column=3, sticky='s')
 
-    def progress(self):
-        progress_bar = ttk.Progressbar(root, orient="vertical", mode="determinate", maximum=100, value=0)
+    def choose_syrup(self, syrup):
+        if (syrup == self.no_syrup):
+            self.syrup_selected['no_syrup'] = True
+            self.syrup_selected['cherry'] = False
+            self.syrup_selected['lemon'] = False
+        if (syrup == self.cherry):
+            self.syrup_selected['no_syrup'] = False
+            self.syrup_selected['cherry'] = True
+            self.syrup_selected['lemon'] = False
+        if (syrup == self.lemon):
+            self.syrup_selected['no_syrup'] = False
+            self.syrup_selected['cherry'] = False
+            self.syrup_selected['lemon'] = True
+
+    def fill_glass(self):
+        color = None
+        if (self.syrup_selected['no_syrup']):
+            color = '#FFEFD5'
+            self.cash-= self.price['no_syrup']
+        if (self.syrup_selected['lemon']):
+            color = 'yellow'
+            self.cash -= self.price['lemon']
+        if (self.syrup_selected['cherry']):
+            color = 'red'
+            self.cash -= self.price['cherry']
+        self.progress(color)
+
+    def progress(self, color):
+        s = ttk.Style()
+       # s.theme_use('clam')
+        s.configure('TProgressbar', background=color)
+        progress_bar = ttk.Progressbar(self.master, style='TProgressbar', orient="vertical", mode="determinate",
+                                       maximum=100, value=0)
         progress_bar.grid(row=10, column=5)
-        self.a -= 10
-        self.label_money.config(text=self.a)
+        self.cash -= 10
+        self.label_money.config(text=self.cash)
         for i in range(101):
             progress_bar.configure(value=i)
             progress_bar.update()
@@ -57,7 +101,11 @@ class Main(tk.Frame):
         progress_bar.grid_forget()
 
 
-if __name__ == "__main__":
+def main():
     root = tk.Tk()
-    app = Main()
+    app = Main(root)
     root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
